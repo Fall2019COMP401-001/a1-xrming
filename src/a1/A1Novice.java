12ba3c2eedaf4dbce225d79c9ecc9706ea7df529
ChristyMing <ChristyMing@xinrandembp.wireless-1x.unc.edu>
2019-08-31 21:01:47 -0400

    new changes in novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,42 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int people = scan.nextInt();
+		
+		double[] total = new double[people];
+		char[] firstName = new char[people];
+		String[] lastName = new String[people];
+		
+		for (int i=0; i<total.length; i++) {
+			String first = scan.next();
+			firstName[i] = first.charAt(0);
+			lastName[i] = scan.next();
+			int items = scan.nextInt();
+			double[] list = new double[items];
+						
+			for (int j=0; j<total.length; j++) {
+
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				Double price = scan.nextDouble();
+				list[j] = quantity * price;
+			}
+			total[i] = sumIt(list);
+			
+			System.out.println(firstName[i] + ". " + lastName[i] + ": " + total[i]);
+		}
+		
 		
 	}
+	
+	static double sumIt(double[] list) {
+		
+		double sum = 0;
+		for (int i=0; i<list.length; i++) {
+			sum += list[i];
+		}
+		return sum;
+	}
+
+	
 }
