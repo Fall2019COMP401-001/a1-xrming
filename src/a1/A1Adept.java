27c9321a415b2070a51de9c9b6c049ba3f323749
ChristyMing <ChristyMing@xinrandembp.wireless-1x.unc.edu>
2019-09-01 11:48:37 -0400

    adept first edition

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -3,12 +3,95 @@ package a1;
 import java.util.Scanner;
 
 public class A1Adept {
-
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int item = scan.nextInt();
+		
+		String[] itemName = new String[item];
+		double[] itemPrice = new double[item];
+		
+		for (int i=0; i < item; i++) {
+			itemName[i] = scan.next();
+			itemPrice[i] = scan.nextDouble();
+		}
+		
+		
+		int customers = scan.nextInt();
+		
+		double [] total = new double[customers];
+		String[] cusname = new String[customers];
+		
+		for (int i=0; i < total.length; i++) {
+			cusname[i] = scan.next() + scan.next();
+			int cart = scan.nextInt();
+			
+			for (int j=0; j < cart; j++) {
+				int howMany = scan.nextInt();
+				String what = scan.next();
+				for (int k=0; k < item; k++) {
+					if (itemName[k] == what) {
+						total[i] += itemPrice[k] * howMany;
+					}
+				}
+			}
+		}
+		
+		String average = calculateValueAve(total, cusname);
+		String min = findValueMin(total, cusname);
+		String max = findValueMax(total, cusname);
+		
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(average);
+
 	}
+	
+	
+	
+	static String calculateValueAve(double[] total, String[] cusname) {
+		
+		double sum = 0;
+		
+		for (int i=0; i<total.length; i++) {
+			sum += total[i];
+		}
+		double ave = sum/total.length;
+		return "Average: " + ave;
+	}
+	
+
+	
+	static String findValueMin(double[] total, String[] cusname) {
+		
+		double min = total[0];
+		int whichOne = 0;
+		
+		for (int i=0; i<total.length; i++) {
+			if (total[i] < min) {
+				min = total[i];
+				whichOne = i;
+			}
+		}
+		return "Smallest: " + cusname[whichOne] + " (" + min + ")";
+	}
+	
+	
+	static String findValueMax(double[] total, String[] cusname) {
+		
+		double max = total[0];
+		int whichOne = 0;
+		
+		for (int i=0; i<total.length; i++) {
+			if (total[i] > max) {
+				max = total[i];
+				whichOne = i;
+			}
+		}
+		return "Biggest: " + cusname[whichOne] + " (" + max + ")";
+	}
+	
+
 }
